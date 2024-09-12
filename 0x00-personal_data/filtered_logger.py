dbafbed8907a0f6fa047991bc432a2c9575c9446
hidaya <hidaya.regragui@gmail.com>
2024-09-12 19:36:37 +0100

    task0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+returns the log message obfuscated
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates the values of specified fields in a log message.
+    """
+    pattern = '|'.join([f'{field}=.*?{separator}' for field in fields])
+    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}{separator}", message)
