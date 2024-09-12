eaab8c8b8d5973eadcee75e7bc2cf8f4250a87af
hidaya <hidaya.regragui@gmail.com>
2024-09-12 20:25:06 +0100

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,11 +6,14 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt and returns the salted, hashed password.
-    
-    :param password: The password to hash (string).
-    :return: The hashed password (byte string).
     """
-    # Generate a salt and hash the password
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against a hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
