aca48968f2590d63337470ed392bcd3f6c146504
hidaya <hidaya.regragui@gmail.com>
2024-09-18 10:10:43 +0100

    task5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Returns the authorization header """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns the current user """
