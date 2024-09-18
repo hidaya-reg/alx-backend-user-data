72aa7b3e442d57a6472bbd27bc47d346db7b5622
hidaya <hidaya.regragui@gmail.com>
2024-09-18 10:16:46 +0100

    task6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class that inherits from Auth """
+    pass
