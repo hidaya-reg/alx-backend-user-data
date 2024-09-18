e3e72e1cc717f9c6be2ba7d35ad3a1eebf1ea8c9
hidaya <hidaya.regragui@gmail.com>
2024-09-18 10:25:36 +0100

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,12 @@
 """ Module for Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Authentication class that inherits from Auth """
+
     def extract_base64_authorization_header(
         self, authorization_header: str
     ) -> str:
@@ -17,3 +19,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Decodes the Base64 string to its UTF-8 value."""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
