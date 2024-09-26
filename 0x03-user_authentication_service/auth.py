eed3ea824455e5c93e90e21fafece859218dcdea
hidaya <hidaya.regragui@gmail.com>
2024-09-26 20:17:08 +0100

    task5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,5 +30,6 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists.")
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            return self._db.add_user(email, hashed_password)
+            pass
+        hashed_password = _hash_password(password)
+        return self._db.add_user(email, hashed_password)
