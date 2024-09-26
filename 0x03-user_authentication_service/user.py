fa22eee80bcd368d6106078d8c91544dfa214082
hidaya <hidaya.regragui@gmail.com>
2024-09-26 18:57:47 +0100

    atsk0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
