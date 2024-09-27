987f712fa01a0d3d4d7932204e769b9a84884a09
hidaya <hidaya.regragui@gmail.com>
2024-09-27 09:24:03 +0100

    task14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,5 +42,17 @@ def login():
     return response
 
 
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """ Logout endpoint
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
