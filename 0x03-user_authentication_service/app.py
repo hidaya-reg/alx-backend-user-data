f71442b8215a3a1a1d28191a0c48814d1d1343b1
hidaya <hidaya.regragui@gmail.com>
2024-09-27 09:26:46 +0100

    task15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,5 +54,15 @@ def logout():
     return redirect(url_for("index"))
 
 
+@app.route("/profile")
+def profile() -> str:
+    """ User profile endpoint
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
