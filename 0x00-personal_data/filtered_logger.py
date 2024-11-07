2360866d8fe247ab7f9e1f41e236543403886374
hidaya <hidaya.regragui@gmail.com>
2024-11-07 18:30:55 +0100

    task1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ obfuscat log message
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -23,3 +24,27 @@ def filter_datum(
         lambda m: f"{m.group(0).split('=')[0]}={redaction}{separator}",
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize with a list of fields to redact """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in log records using filter_datum """
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.msg,
+            self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
