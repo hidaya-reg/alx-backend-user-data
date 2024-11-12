ed4fc447ddd445b67cffcce4b5f33ad5cc6ab481
hidaya <hidaya.regragui@gmail.com>
2024-11-12 15:10:41 +0100

    task2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,11 @@ def stats() -> str:
 def unauthorized() -> None:
     """ Raise a 401 error """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Raise a 403 error
+    """
+    abort(403)
