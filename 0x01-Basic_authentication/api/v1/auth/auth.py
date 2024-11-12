10a092f21663b29183b64b92e3d517198e00eb4f
hidaya <hidaya.regragui@gmail.com>
2024-11-12 15:18:03 +0100

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,17 +3,30 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """ A template for all authentication systems. """
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if authentication is required. """
-        return False
-    
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        return path not in excluded_paths
+
     def authorization_header(self, request=None) -> str:
         """ Retrieves the authorization header from the request. """
         return None
-    
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the authorization header from the request. """
+        return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retrieves the current user. """
         return None
