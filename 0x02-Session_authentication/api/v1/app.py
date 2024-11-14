2d14af46c8b1c33048d1d0d3cd7269ed2d65271f
hidaya <hidaya.regragui@gmail.com>
2024-11-14 15:16:24 +0100

    task6

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,7 +44,7 @@ def before_request():
 
         # Check for the Authorization header
         if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
+                auth.session_cookie(request) is None):
             abort(401)
 
         request.current_user = auth.current_user(request)
